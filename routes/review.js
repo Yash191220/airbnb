6b62c262b544ac54e809c85ad4407aef7670781c
Yashraj Narayan Prasad <yashraj191220@gmail.com>
2026-01-11 22:59:49 +0530

    Fix error handling in review validation

--- routes/review.js
+++ routes/review.js
@@ -13,7 +13,7 @@ const validateReview = (req,res,next) =>{
   let {error} = reviewSchema.validate(req.body);
   if(error){
     let errMsg = error.details.map((el)=>el.message).join(",");
-    throw new ExpressError(400,errMsg);
+    return next(new ExpressError(400, errMsg));
   }else{
     next();
   }
